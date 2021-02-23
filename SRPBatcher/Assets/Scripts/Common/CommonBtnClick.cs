5c93f07105f6271cf0f79d1b93f68c89540977c9
sunmaoqi <sunmaoqi@fotoable.com>
2021-02-23 18:33:13 +0800

    add ui

---
+++ SRPBatcher/Assets/Scripts/Common/CommonBtnClick.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class CommonBtnClick : MonoBehaviour
+{
+    public void OnBtnClick_Print()
+    {
+        var image = this.transform.GetComponent<Image>();
+        image.color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+        Debug.Log(this.name + " 's color is (" + image.color.r + "," + image.color.g + "," + image.color.b + ").");
+    }
+}
