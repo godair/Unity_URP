4bcc7e1760e50fac930b08730340829d75f1277f
sunmaoqi <sunmaoqi@fotoable.com>
2021-02-24 11:44:55 +0800

    coo 相机组合

--- SRPBatcher/Assets/Scripts/Common/CommonBtnClick.cs
+++ SRPBatcher/Assets/Scripts/Common/CommonBtnClick.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Random = UnityEngine.Random;
+using GAsset.Common;
 
 public class CommonBtnClick : MonoBehaviour
 {
     public void OnBtnClick_Print()
     {
         var image = this.transform.GetComponent<Image>();
-        image.color = new Color(Random.Range(0, 1), Random.Range(0, 1), Random.Range(0, 1));
+        image.color = new Color(CommonFunction.GetRandomfloat(0, 1.0f, 2), CommonFunction.GetRandomfloat(0, 1.0f, 2), CommonFunction.GetRandomfloat(0, 1.0f, 2));
         Debug.Log(this.name + " 's color is (" + image.color.r + "," + image.color.g + "," + image.color.b + ").");
     }
 }
---
+++ SRPBatcher/Assets/Scripts/Common/CommonFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GAsset.Common
+{
+    public static class CommonFunction
+    {
+        public static float GetRandomfloat(float min, float max, int deci = 0)
+        {
+            deci = 10 * deci;
+            float ranf = Random.Range(min, max);
+            if (deci != 0)
+            {
+                ranf = Mathf.Round(ranf * deci) / deci;
+            }
+            return ranf;
+        }
+    }
+}
